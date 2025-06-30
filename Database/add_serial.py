d07c37b8349e498abbed58c1a58ebc205612eb21
Abbos <abboszafarxojaev@gmail.com>
2025-06-30 12:14:02 +0500

    none xatosi to'g'irlandi

--- Database/add_serial.py
+++ Database/add_serial.py
@@ -18,12 +18,31 @@ def add_serial_db(data):
     conn = get_connection()
     cur = conn.cursor()
 
-    cur.execute(f"insert into serial (serial_id, serial_name, serial_language, serial_janr, serial_fasl, serial_year, qism_id, serial_qism, video_id, second)"
-                f"values ({data['serial_info']['serial_id']}, '{data['serial_info']['serial_name']}', '{data['serial_info']['serial_language']}', '{data['serial_info']['serial_janr']}', {data['serial_fasl']}, {data['serial_info']['serial_year']}, {data['qism_id']}, {data['serial_qism']}, '{data['video_id']}', {data['second']});")
+    cur.execute(
+        """
+        INSERT INTO serial (
+            serial_id, serial_name, serial_language, serial_janr,
+            serial_fasl, serial_year, qism_id, serial_qism, video_id, second
+        )
+        VALUES (%s, %s, %s, %s, %s, %s, %s, %s, %s, %s)
+        """,
+        (
+            data['serial_info']['serial_id'],
+            data['serial_info']['serial_name'],
+            data['serial_info']['serial_language'],
+            data['serial_info']['serial_janr'],
+            data['serial_fasl'],
+            data['serial_info']['serial_year'],
+            data['qism_id'],
+            data['serial_qism'],
+            data['video_id'],
+            data['second']
+        )
+    )
 
     conn.commit()
-
     cur.close()
     conn.close()
+
 
 
